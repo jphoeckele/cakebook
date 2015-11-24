415b49f4c53a79ee036d43960bfd7066e7dca0dc
jphoeckele <jphoeckele@gmail.com>
2015-11-24 14:54:25 -0600

    set up basic gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+	//Project configuration
+	grunt.initConfig({
+		/*
+		* We will configure our tasks here
+		*/
+	});
+
+	// We will load Grunt plugins here
+
+	// We will register tasks here
+};
