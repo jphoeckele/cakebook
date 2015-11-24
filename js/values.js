ee301cea1f6a44c48acb1dba8f311dd9c90e5ea5
jphoeckele <jphoeckele@gmail.com>
2015-11-24 14:42:53 -0600

    created a release directory and package.json file

--- js/values.js
+++ js/values.js
@@ -1,9 +1,11 @@
 var projectName = "cakebook";
 var versionNumber = '0.0.1';
-var currentDate = new Date();	//Create Date object. More about objects and
-															//Date objects in chapter 5. This object will
-															//be used to builf our date.
-	//currentTime will look like '2015-11-24 at 12:30:30'
+var currentDate = new Date();
+//Create Date object. More about objects and
+//Date objects in chapter 5. This object will
+//be used to builf our date.
+
+//currentTime will look like '2015-11-24 at 12:30:30'
 var currentTime = currentDate.getFullYear() + '-' + 		//Set year
 									(currentDate.getMonth() + 1)+ '-' +	  //Set month
 									currentDate.getDate() + ' at ' + 			//Set day of the month
---
+++ release/main.js
@@ -0,0 +1,18 @@
+var projectName = "cakebook";
+var versionNumber = '0.0.1';
+var currentDate = new Date();
+//Create Date object. More about objects and
+//Date objects in chapter 5. This object will
+//be used to builf our date.
+
+//currentTime will look like '2015-11-24 at 12:30:30'
+var currentTime = currentDate.getFullYear() + '-' + 		//Set year
+									(currentDate.getMonth() + 1)+ '-' +	  //Set month
+									currentDate.getDate() + ' at ' + 			//Set day of the month
+									currentDate.getHours() + ':' + 				//Set hours
+									currentDate.getMinutes() + ':' + 			//Set minutes
+									currentDate.getSeconds();							//Set seconds
+var userName = prompt("Hello, what's your name?");
+document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>" +
+													'<p>' + projectName + ' ' + versionNumber +
+													' accesseed on: ' + currentTime + '</p>';
