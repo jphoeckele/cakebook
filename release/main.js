fa7d96580fdb0d5715dfeaccc276eb94eac9e590
jphoeckele <jphoeckele@gmail.com>
2015-11-25 11:14:35 -0600

    created phone number regex authentication which is prompted after name is enter.

--- release/main.js
+++ release/main.js
@@ -19,9 +19,23 @@ var cbValues = {
 	currentTime: currentTime
 };
 
+//Get the user's name.
 var userName = prompt("Hello, what's your name?");
-document.body.innerHTML = "<h1>Hello, " + userName + "!</h1>" +
-													'<p>' + cbValues.projectName + ' ' + cbValues.versionNumber +
-													' viewed on: ' + cbValues.currentTime + '</p>';
+//Get the user's phone number.
+var phoneNumber = prompt("Hello " + userName + ", What is your phone number?");
+//Phone number Regex pattern
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+//Variable to store output
+var output = "<h1>Hello, " + userName + "!</h1>";
+//Check's is phone number is valid
+if (phoneNumberPattern.test(phoneNumber)) {
+	//Yes number is valid
+	output = output + '<p>' + cbValues.projectName + " " + cbValues.versionNumber + ' viewed on: ' + cbValues.currentTime + '</p>';
+} else {
+	//No number is not valid
+	output = output + "<h2>I'm sorry but that phone number is not valid: " + phoneNumber;
+}
+//Insert the output into the web page.
+document.body.innerHTML = output;
 
 var images = document.querySelectorAll('div._1dwg');
